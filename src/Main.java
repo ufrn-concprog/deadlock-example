f69505b8a07df9e3d21c40bdfc806cfbbf30fbb5
Everton Cavalcante <evertonranielly@gmail.com>
2025-08-31 23:50:58 -0300

    Adding example to GitHub

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Main program
+ * @author <a href="mailto:everton.cavalcante@ufrn.br">Everton Cavalcante</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        final Resource resourceA = new Resource("Resource A");
+        final Resource resourceB = new Resource("Resource B");
+
+        // Thread T1 is created with Resource A assigned and wants to hold Resource B
+        // Thread T2 is created with Resource B assigned and wants to hold Resource A
+        // This represents a deadlock
+        Thread t1 = new Thread(() -> resourceA.use(resourceB), "Thread T1");
+        Thread t2 = new Thread(() -> resourceB.use(resourceA), "Thread T2");
+
+        t1.start();
+        t2.start();
+    }
+}
---
+++ src/Resource.java
@@ -0,0 +1,45 @@
+/**
+ * Class representing a resource to be held by a thread
+ * @author <a href="mailto:everton.cavalcante@ufrn.br">Everton Cavalcante</a>
+ */
+public class Resource {
+    /** A name for the resource */
+    private final String name;
+
+    /**
+     * Parameterized constructor
+     * @param name Name to assign to the resource
+     */
+    public Resource(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Representation of the use of the resource by a thread. This method executes in mutual exclusion due to
+     * the use of the <em>synchronized</em> keyword.
+     * When a method or a block of code is declared as synchronized, like this one, Java acquires a lock on a
+     * specific object (in this case, <em>resource</em>), and only the thread that holds this lock can execute the
+     * synchronized code. Other threads attempting to enter the synchronized section will be blocked until the
+     * lock is released by the currently executing thread.
+     * @param resource Reference to the resource
+     */
+    synchronized void use(final Resource resource) {
+        System.out.println(Thread.currentThread().getName() + " is attempting to lock " + this.name);
+        synchronized (this) {
+            System.out.println(Thread.currentThread().getName() + " locked " + this.name);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + " is attempting to lock " + resource);
+            synchronized (resource) {
+                System.out.println(resource);
+            }
+        }
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
